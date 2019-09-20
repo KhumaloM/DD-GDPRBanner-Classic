1d8ed19070b6d07376e4f0aedfbc73eb3f318d9a
Mthabisi Khumalo <Mthabisi.Khumalo@dimensiondata.com>
2019-09-20 12:34:27 +0200

    Initial check in

--- DD-GDPRBanner-ClassicWeb/Pages/Default.aspx.cs
+++ DD-GDPRBanner-ClassicWeb/Pages/Default.aspx.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.SharePoint.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -14,30 +16,129 @@ namespace DD_GDPRBanner_ClassicWeb
             Uri redirectUrl;
             switch (SharePointContextProvider.CheckRedirectionStatus(Context, out redirectUrl))
             {
-                case RedirectionStatus.Ok:
-                    return;
-                case RedirectionStatus.ShouldRedirect:
-                    Response.Redirect(redirectUrl.AbsoluteUri, endResponse: true);
-                    break;
-                case RedirectionStatus.CanNotRedirect:
-                    Response.Write("An error occurred while processing your request.");
-                    Response.End();
-                    break;
+                //case RedirectionStatus.Ok:
+                //    return;
+                //case RedirectionStatus.ShouldRedirect:
+                //    Response.Redirect(redirectUrl.AbsoluteUri, endResponse: true);
+                //    break;
+                //case RedirectionStatus.CanNotRedirect:
+                //    Response.Write("An error occurred while processing your request.");
+                //    Response.End();
+                //    break;
             }
         }
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            // The following code gets the client context and Title property by using TokenHelper.
-            // To access other properties, the app may need to request permissions on the host web.
+            // define initial script, needed to render the chrome control
+            string script = @"
+            function chromeLoaded() {
+                $('body').show();
+            }
+
+            //function callback to render chrome after SP.UI.Controls.js loads
+            function renderSPChrome() {
+                //Set the chrome options for launching Help, Account, and Contact pages
+                var options = {
+                    'appTitle': document.title,
+                    'onCssLoaded': 'chromeLoaded()'
+                };
+
+                //Load the Chrome Control in the divSPChrome element of the page
+                var chromeNavigation = new SP.UI.Controls.Navigation('divSPChrome', options);
+                chromeNavigation.setVisible(true);
+            }";
+
+            //register script in page
+            Page.ClientScript.RegisterClientScriptBlock(typeof(Default), "BasePageScript", script, true);
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Trace.TraceInformation("Embeding started");
+            var spContext = SharePointContextProvider.Current.GetSharePointContext(Context);
+            if(spContext==null)
+            {
+                System.Diagnostics.Trace.TraceInformation("spContext is null");
+            }
+            else
+            {
+                System.Diagnostics.Trace.TraceInformation("spContext is NOT null");
+            }
+            using (var ctx = spContext.CreateUserClientContextForSPHost())
+            {
+                AddJsLink(ctx, ctx.Web);
+            }
+        }
+
+        protected void btnRemove_Click(object sender, EventArgs e)
+        {
             var spContext = SharePointContextProvider.Current.GetSharePointContext(Context);
 
-            using (var clientContext = spContext.CreateUserClientContextForSPHost())
+            using (var ctx = spContext.CreateUserClientContextForSPHost())
             {
-                clientContext.Load(clientContext.Web, web => web.Title);
-                clientContext.ExecuteQuery();
-                Response.Write(clientContext.Web.Title);
+                DeleteJsLink(ctx, ctx.Web);
             }
+        }
+
+        public void AddJsLink(ClientContext ctx, Web web)
+        {
+            string scenarioUrl = String.Format("{0}://{1}:{2}/Scripts", this.Request.Url.Scheme,
+                                                this.Request.Url.DnsSafeHost, this.Request.Url.Port);
+            string revision = Guid.NewGuid().ToString().Replace("-", "");
+            string jsLink = string.Format("{0}/{1}?rev={2}", scenarioUrl, "scenario1.js", revision);
+
+            StringBuilder scripts = new StringBuilder(@"
+                var headID = document.getElementsByTagName('head')[0]; 
+                var");
+
+            scripts.AppendFormat(@"
+                newScript = document.createElement('script');
+                newScript.type = 'text/javascript';
+                newScript.src = '{0}';
+                headID.appendChild(newScript);", jsLink);
+            string scriptBlock = scripts.ToString();
+
+            var existingActions = web.UserCustomActions;
+            ctx.Load(existingActions);
+            ctx.ExecuteQuery();
+            var actions = existingActions.ToArray();
+            foreach (var action in actions)
+            {
+                if (action.Description == "scenario1" &&
+                    action.Location == "ScriptLink")
+                {
+                    action.DeleteObject();
+                    ctx.ExecuteQuery();
+                }
+            }
+
+            var newAction = existingActions.Add();
+            newAction.Description = "scenario1";
+            newAction.Location = "ScriptLink";
+
+            newAction.ScriptBlock = scriptBlock;
+            newAction.Update();
+            ctx.Load(web, s => s.UserCustomActions);
+            ctx.ExecuteQuery();
+        }
+
+        public void DeleteJsLink(ClientContext ctx, Web web)
+        {
+            var existingActions = web.UserCustomActions;
+            ctx.Load(existingActions);
+            ctx.ExecuteQuery();
+            var actions = existingActions.ToArray();
+            foreach (var action in actions)
+            {
+                if (action.Description == "scenario1" &&
+                    action.Location == "ScriptLink")
+                {
+                    action.DeleteObject();
+                    ctx.ExecuteQuery();
+                }
+            }
+
         }
     }
 }
